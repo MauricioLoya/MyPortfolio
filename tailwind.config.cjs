ea132be8bb9dcfaee48ff0901256c309c3f44eea
MauricioLoya <mauloy15@gmail.com>
2023-07-12 21:52:48 -0600

    new font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,14 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
-      },
-    },
+        sans: ["'Martian Mono', monospace", ...defaultTheme.fontFamily.sans]
+      }
+    }
   },
-  plugins: [],
-};
+  plugins: []
+}
