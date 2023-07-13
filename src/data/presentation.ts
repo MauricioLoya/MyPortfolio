5dee0710ed933a091c26113d7618c4bb8fb741ce
MauricioLoya <mauloy15@gmail.com>
2023-07-12 21:53:33 -0600

    new link to presentation

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -14,11 +14,15 @@ const presentation: Presentation = {
   mail: 'mauloy15@gmail.com',
   title: 'Hi, I’m Mauricio Loya',
   description:
-    'Experienced Software Developer with *nearly 4 years of expertise* n the field. Proficient in multiple programming languages and dedicated to delivering high-quality code. Skilled in problem-solving and collaborating with cross-functional teams to achieve projectobjectives. Passionate about innovation and continuously improving technical skills.Ready to contribute to the success of software development initiatives.',
+    'Experienced Software Developer with * 4 years of expertise* n the field. Proficient in multiple programming languages and dedicated to delivering high-quality code. Skilled in problem-solving and collaborating with cross-functional teams to achieve projectobjectives. Passionate about innovation and continuously improving technical skills.Ready to contribute to the success of software development initiatives.',
   socials: [
     {
       label: 'Github',
       link: 'https://github.com/MauricioLoya'
+    },
+    {
+      label: 'stack overflow',
+      link: 'https://stackoverflow.com/users/13255504/mauricio-loya'
     }
   ]
 }
